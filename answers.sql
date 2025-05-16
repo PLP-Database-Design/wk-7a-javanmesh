225795c24b2d6dd27bca067057e760dc67abb958
Javan Meshack <javanmeshac@gmail.com>
2025-05-16 12:06:33 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,31 +1,44 @@
---Question 1
--- Query: Split comma-separated Products into atomic rows using JSON_TABLE()
-SELECT
-  OrderID,
-  CustomerName,
-  TRIM(j.Product) AS Product
-FROM
-  ProductDetail
-  JOIN JSON_TABLE(
-    CONCAT('["', REPLACE(Products, ', ', '","'), '"]'),
-    '$[*]' COLUMNS (
-      Product VARCHAR(100) PATH '$'
-    )
-  ) AS j;
-
+ --  Question 1
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
 -- Question 2
--- Step 1: Create Orders table (removes partial dependency)
-CREATE TABLE Orders AS
-SELECT DISTINCT
-  OrderID,
-  CustomerName
-FROM OrderDetails1NF;
+ CREATE TABLE orders(
+OrderID INT PRIMARY KEY,
+customerName VARCHAR(100)
+);
+INSERT INTO orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
--- Step 2: Create OrderItems table (purely dependent on full key)
-CREATE TABLE OrderItems AS
-SELECT
-  OrderID,
-  Product,
-  Quantity
-FROM OrderDetails1NF;
+-- Product  table 
+CREATE TABLE product(
+product_id INT primary key,
+productName varchar(100),
+quantity INT,
+order_id INT,
+foreign key(order_id) references orders(OrderID)
+);
+
+insert into product(product_id,productName,quantity,order_id)
+values 
+(1,'laptop',2,101),
+(2,'Mouse',1,101),
+(3,'Tablet',3,102),
+(4,'Keyboard',2,102),
+(5,'Mouse',1,102),
+(6,'Phone',1,103);
+
 
